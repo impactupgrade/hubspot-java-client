c327b8e55bfc3b4f9ef723d490b1690ade379bc1
Brett Meyer <brett@3riverdev.com>
2020-06-29 13:03:15 -0400

    disabling aggressive logging

--- src/main/java/com/impactupgrade/integration/hubspot/AbstractClient.java
+++ src/main/java/com/impactupgrade/integration/hubspot/AbstractClient.java
@@ -1,13 +1,8 @@
 package com.impactupgrade.integration.hubspot;
 
-import org.glassfish.jersey.logging.LoggingFeature;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Feature;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public abstract class AbstractClient {
 
@@ -18,12 +13,12 @@ public abstract class AbstractClient {
   protected AbstractClient(String apiKey) {
     this.apiKey = apiKey;
 
-    Logger logger = Logger.getLogger(this.getClass().getName());
-    Feature loggingFeature = new LoggingFeature(
-        logger, Level.FINE, LoggingFeature.Verbosity.PAYLOAD_TEXT, LoggingFeature.DEFAULT_MAX_ENTITY_SIZE);
+//    Logger logger = Logger.getLogger(this.getClass().getName());
+//    Feature loggingFeature = new LoggingFeature(
+//        logger, Level.FINE, LoggingFeature.Verbosity.PAYLOAD_TEXT, LoggingFeature.DEFAULT_MAX_ENTITY_SIZE);
 
     Client client = ClientBuilder.newBuilder()
-        .register(loggingFeature)
+//        .register(loggingFeature)
         .build();
 
     target = client.target("https://api.hubapi.com");
