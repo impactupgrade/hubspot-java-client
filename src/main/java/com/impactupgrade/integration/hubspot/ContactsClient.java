f7ba368f03fb07307c7f9f5458fe448ffd896be0
Landon Baer <baerl@huntington.edu>
2020-08-06 17:00:02 -0400

    update contact in HubSpot

--- src/main/java/com/impactupgrade/integration/hubspot/ContactsClient.java
+++ src/main/java/com/impactupgrade/integration/hubspot/ContactsClient.java
@@ -1,9 +1,12 @@
 package com.impactupgrade.integration.hubspot;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.impactupgrade.integration.hubspot.builder.ContactBuilder;
 import com.impactupgrade.integration.hubspot.exception.DuplicateContactException;
 import com.impactupgrade.integration.hubspot.exception.HubSpotException;
 import com.impactupgrade.integration.hubspot.model.Contact;
+import com.impactupgrade.integration.hubspot.model.ContactRequest;
 import com.impactupgrade.integration.hubspot.model.internal.ErrorResponse;
 
 import javax.ws.rs.client.Entity;
@@ -11,7 +14,12 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ContactsClient extends AbstractClient {
+
+  private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
   private final WebTarget contactsTarget = target.path("contacts/v1");
 
@@ -20,11 +28,13 @@ public class ContactsClient extends AbstractClient {
   }
 
   public Contact getByEmail(String email) {
-    return contactsTarget
+    Contact c = contactsTarget
         .path("contact/email/" + email + "/profile")
         .queryParam("hapikey", apiKey)
         .request(MediaType.APPLICATION_JSON)
         .get(Contact.class);
+    LOGGER.log(Level.INFO, c.toString());
+    return c;
   }
 
   public Contact insert(ContactBuilder contactBuilder) throws DuplicateContactException, HubSpotException {
@@ -43,5 +53,73 @@ public class ContactsClient extends AbstractClient {
     } else {
       return response.readEntity(Contact.class);
     }
+  }
+
+  public Contact update(
+    String email,
+    String first_name,
+    String last_name,
+    String home_phone,
+    String mobile_phone,
+    String work_phone,
+    String preferred_phone,
+    String address,
+    String city,
+    String state,
+    String zip,
+    String country) {
+
+    ContactBuilder cBuilder = new ContactBuilder();
+    cBuilder.firstName(first_name);
+    LOGGER.log(Level.INFO, first_name);
+    cBuilder.lastName(last_name);
+    LOGGER.log(Level.INFO, last_name);
+
+    cBuilder.home_phone(home_phone);
+    LOGGER.log(Level.INFO, home_phone);
+    cBuilder.mobile_phone(mobile_phone);
+    LOGGER.log(Level.INFO, mobile_phone);
+    cBuilder.work_phone(work_phone);
+    LOGGER.log(Level.INFO, work_phone);
+    cBuilder.preferred_phone(preferred_phone);
+    LOGGER.log(Level.INFO, preferred_phone);
+
+    if (preferred_phone == "Home") {
+      cBuilder.phone(home_phone);
+    } else if (preferred_phone == "Mobile") {
+      cBuilder.phone(mobile_phone);
+    } else if (preferred_phone == "Work") {
+      cBuilder.phone(work_phone);
+    }
+
+    cBuilder.address(address);
+    LOGGER.log(Level.INFO, address);
+    cBuilder.city(city);
+    LOGGER.log(Level.INFO, city);
+    cBuilder.state(state);
+    LOGGER.log(Level.INFO, state);
+    cBuilder.zip(zip);
+    LOGGER.log(Level.INFO, zip);
+    cBuilder.country(country);
+    LOGGER.log(Level.INFO, country);
+    ContactRequest cr = cBuilder.build();
+
+    // Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    // String contactUpdate = gson.toJson(cr);
+    // LOGGER.log(Level.INFO, contactUpdate);
+
+    Response response = contactsTarget
+        .path("contact/email/" + email + "/profile")
+        .queryParam("hapikey", apiKey)
+        .request(MediaType.APPLICATION_JSON)
+        .post(Entity.entity(cr, MediaType.APPLICATION_JSON));
+
+    LOGGER.log(Level.INFO, "Updating contact: " + first_name + " " + last_name);
+      
+    return response.readEntity(Contact.class);
+  }
+
+  public static void main(String[] args) {
+    // new ContactsClient("56fac03b-0260-411b-9f8d-ad9c77746da9").getByEmail("brett.e.meyer@gmail.com");
   }
 }
--- src/main/java/com/impactupgrade/integration/hubspot/model/ContactProperties.java
+++ src/main/java/com/impactupgrade/integration/hubspot/model/ContactProperties.java
@@ -6,6 +6,15 @@ public class ContactProperties implements AbstractModel {
   private HasValue<String> lastname;
   private HasValue<String> email;
   private HasValue<String> phone;
+  private HasValue<String> home_phone;
+  private HasValue<String> mobile_phone;
+  private HasValue<String> work_phone;
+  private HasValue<String> preferred_phone;
+  private HasValue<String> address;
+  private HasValue<String> city;
+  private HasValue<String> state;
+  private HasValue<String> zip;
+  private HasValue<String> country;
 
   public HasValue<String> getFirstname() {
     return firstname;
@@ -39,6 +48,78 @@ public class ContactProperties implements AbstractModel {
     this.phone = phone;
   }
 
+  public HasValue<String> getHome_Phone() {
+    return home_phone;
+  }
+
+  public void setHome_Phone(HasValue<String> home_phone) {
+    this.home_phone = home_phone;
+  }
+
+  public HasValue<String> getWork_Phone() {
+    return work_phone;
+  }
+
+  public void setWork_Phone(HasValue<String> work_phone) {
+    this.work_phone = work_phone;
+  }
+
+  public HasValue<String> getMobile_Phone() {
+    return mobile_phone;
+  }
+
+  public void setMobile_Phone(HasValue<String> mobile_phone) {
+    this.mobile_phone = mobile_phone;
+  }
+
+  public HasValue<String> getPreferred_Phone() {
+    return preferred_phone;
+  }
+
+  public void setPreferred_Phone(HasValue<String> preferred_phone) {
+    this.preferred_phone = preferred_phone;
+  }
+
+  public HasValue<String> getAddress() {
+    return address;
+  }
+
+  public void setAddress(HasValue<String> address) {
+    this.address = address;
+  }
+
+  public HasValue<String> getCity() {
+    return city;
+  }
+
+  public void setCity(HasValue<String> city) {
+    this.city = city;
+  }
+
+  public HasValue<String> getState() {
+    return state;
+  }
+
+  public void setState(HasValue<String> state) {
+    this.state = state;
+  }
+
+  public HasValue<String> getZip() {
+    return zip;
+  }
+
+  public void setZip(HasValue<String> zip) {
+    this.zip = zip;
+  }
+
+  public HasValue<String> getCountry() {
+    return country;
+  }
+
+  public void setCountry(HasValue<String> country) {
+    this.country = country;
+  }
+
   @Override
   public String toString() {
     return "ContactProperties{" +
@@ -46,6 +127,15 @@ public class ContactProperties implements AbstractModel {
         ", lastname=" + lastname +
         ", email=" + email +
         ", phone=" + phone +
+        ", home_phone=" + home_phone +
+        ", work_phone=" + work_phone +
+        ", mobile_phone=" + mobile_phone +
+        ", preferred_phone=" + preferred_phone +
+        ", address=" + address +
+        ", city=" + city +
+        ", state=" + state +
+        ", zip=" + zip +
+        ", country=" + country +
         '}';
   }
 }
