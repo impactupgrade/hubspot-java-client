7ade2aaa95bb4a65fb847acd37b55d1663b70b0f
Landon Baer <baerl@huntington.edu>
2020-08-12 11:25:12 -0400

    update contact in HubSpot fixes

--- src/main/java/com/impactupgrade/integration/hubspot/ContactsClient.java
+++ src/main/java/com/impactupgrade/integration/hubspot/ContactsClient.java
@@ -1,7 +1,5 @@
 package com.impactupgrade.integration.hubspot;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.impactupgrade.integration.hubspot.builder.ContactBuilder;
 import com.impactupgrade.integration.hubspot.exception.DuplicateContactException;
 import com.impactupgrade.integration.hubspot.exception.HubSpotException;
@@ -14,12 +12,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class ContactsClient extends AbstractClient {
-
-  private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
   private final WebTarget contactsTarget = target.path("contacts/v1");
 
@@ -33,7 +26,6 @@ public class ContactsClient extends AbstractClient {
         .queryParam("hapikey", apiKey)
         .request(MediaType.APPLICATION_JSON)
         .get(Contact.class);
-    LOGGER.log(Level.INFO, c.toString());
     return c;
   }
 
@@ -55,67 +47,16 @@ public class ContactsClient extends AbstractClient {
     }
   }
 
-  public Contact update(
-    String email,
-    String first_name,
-    String last_name,
-    String home_phone,
-    String mobile_phone,
-    String work_phone,
-    String preferred_phone,
-    String address,
-    String city,
-    String state,
-    String zip,
-    String country) {
+  public Contact update(ContactBuilder contactBuilder, String email) {
 
-    ContactBuilder cBuilder = new ContactBuilder();
-    cBuilder.firstName(first_name);
-    LOGGER.log(Level.INFO, first_name);
-    cBuilder.lastName(last_name);
-    LOGGER.log(Level.INFO, last_name);
-
-    cBuilder.home_phone(home_phone);
-    LOGGER.log(Level.INFO, home_phone);
-    cBuilder.mobile_phone(mobile_phone);
-    LOGGER.log(Level.INFO, mobile_phone);
-    cBuilder.work_phone(work_phone);
-    LOGGER.log(Level.INFO, work_phone);
-    cBuilder.preferred_phone(preferred_phone);
-    LOGGER.log(Level.INFO, preferred_phone);
-
-    if (preferred_phone == "Home") {
-      cBuilder.phone(home_phone);
-    } else if (preferred_phone == "Mobile") {
-      cBuilder.phone(mobile_phone);
-    } else if (preferred_phone == "Work") {
-      cBuilder.phone(work_phone);
-    }
-
-    cBuilder.address(address);
-    LOGGER.log(Level.INFO, address);
-    cBuilder.city(city);
-    LOGGER.log(Level.INFO, city);
-    cBuilder.state(state);
-    LOGGER.log(Level.INFO, state);
-    cBuilder.zip(zip);
-    LOGGER.log(Level.INFO, zip);
-    cBuilder.country(country);
-    LOGGER.log(Level.INFO, country);
-    ContactRequest cr = cBuilder.build();
-
-    // Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    // String contactUpdate = gson.toJson(cr);
-    // LOGGER.log(Level.INFO, contactUpdate);
+    ContactRequest req = contactBuilder.build();
 
     Response response = contactsTarget
         .path("contact/email/" + email + "/profile")
         .queryParam("hapikey", apiKey)
         .request(MediaType.APPLICATION_JSON)
-        .post(Entity.entity(cr, MediaType.APPLICATION_JSON));
+        .post(Entity.entity(req, MediaType.APPLICATION_JSON));
 
-    LOGGER.log(Level.INFO, "Updating contact: " + first_name + " " + last_name);
-      
     return response.readEntity(Contact.class);
   }
 
