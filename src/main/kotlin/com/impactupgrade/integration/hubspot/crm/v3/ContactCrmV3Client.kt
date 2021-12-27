e64216960214aa058e86eed6968d6447857dedf4
Brett Meyer <brett@3riverdev.com>
2021-12-27 12:55:58 -0500

    email EQ search is case sensitive, so lowercase email first

--- src/main/kotlin/com/impactupgrade/integration/hubspot/crm/v3/ContactCrmV3Client.kt
+++ src/main/kotlin/com/impactupgrade/integration/hubspot/crm/v3/ContactCrmV3Client.kt
@@ -79,7 +79,8 @@ class ContactCrmV3Client(apiKey: String) : AbstractCrmV3Client(
     // https://community.hubspot.com/t5/APIs-Integrations/Search-Contacts-for-Secondary-Mail/td-p/358756
     search(
       listOf(
-        FilterGroup(listOf(Filter("email", "EQ", email))),
+        // EQ is case-sensitive!
+        FilterGroup(listOf(Filter("email", "EQ", email.lowercase()))),
         FilterGroup(listOf(Filter("hs_additional_emails", "CONTAINS_TOKEN", email)))
       ),
       customProperties
